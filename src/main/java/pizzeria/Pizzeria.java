cc010f85d8823e531192f69f238c8bf2783c6997
unknown <anaelle.danton@gmail.com>
2020-11-27 20:46:23 +0100

    fix #All: Completed all duties

---
+++ src/main/java/pizzeria/Pizzeria.java
@@ -0,0 +1,33 @@
+package pizzeria;
+
+import pizza.Pizza;
+import pizza.SimpleFabriqueDePizzas;
+
+/**
+ * @author bruel (from O'Reilly Head-First series)
+ * @depend - * - Pizza
+ */
+public abstract class Pizzeria {
+
+	SimpleFabriqueDePizzas fabrique;
+
+	public Pizzeria(SimpleFabriqueDePizzas f){
+		this.fabrique = f;
+	}
+
+	public final Pizza commanderPizza(String type) {
+
+		Pizza pizza;
+
+		pizza = fabrique.creerPizza(type);
+
+		pizza.preparer();
+		pizza.cuire();
+		pizza.couper();
+		pizza.emballer();
+
+		return pizza;
+	}
+
+	protected abstract Pizza creerPizza(String type);
+}
